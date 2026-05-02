a70ca19a67ee68690392713b24415a9cc9050e36
Tiago Davies <tiagocaetanodavies@gmail.com>
2026-05-02 12:13:54 -0500

    Init commit

---
+++ src/game/main.ts
@@ -0,0 +1,47 @@
+import { Boot } from './scenes/Boot';
+import { GameOver } from './scenes/GameOver';
+import { Game as MainGame } from './scenes/Game';
+import { MainMenu } from './scenes/MainMenu';
+import { AUTO, Game ,Scale } from 'phaser';
+import { Preloader } from './scenes/Preloader';
+
+//  Find out more information about the Game Config at:
+//  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1280,
+    height: 720,
+    scale: {
+        mode: Scale.FIT,
+        autoCenter: Scale.CENTER_BOTH,
+        autoRound: true 
+    },
+    fps: {
+        target: 60,
+        forceSetTimeOut: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0, x: 0 },
+            debug: true
+        }
+    },
+    parent: 'game-container',
+    backgroundColor: '#028af8',
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        MainGame,
+        GameOver
+    ]
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
