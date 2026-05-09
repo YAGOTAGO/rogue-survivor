45ca609f849287fa89a57b184ea53771757bb9cf
YAGOTAGO <tiagocaetanodavies@gmail.com>
2026-05-09 01:23:33 -0500

    Update SpriteSyncSystem.ts

--- src/systems/SpriteSyncSystem.ts
+++ src/systems/SpriteSyncSystem.ts
@@ -8,7 +8,6 @@ export const spriteSyncSystem = (world: GameWorld) => {
         if (!sprite) continue
         
         if(Velocity.x[eid] === 0 && Velocity.y[eid] === 0){
-            console.log("snapping to grid")
             sprite.x = Math.round(Position.x[eid])
             sprite.y = Math.round(Position.y[eid])
         }else{
