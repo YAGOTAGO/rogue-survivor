125ae3a7167e718203184ddf2b71c191acf0b6f8
Tiago Davies <tiagocaetanodavies@gmail.com>
2026-05-17 22:36:44 -0500

    Update Preloader.ts

--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -58,7 +58,7 @@ export class Preloader extends Scene
     create ()
     {
         this.anims.createFromAseprite(ASSETS.IMAGES.EXPERIENCE);
-        this.scene.start('MainMenu');
-        // this.scene.start('Game');
+        // this.scene.start('MainMenu');
+        this.scene.start('Game');
     }
 }
