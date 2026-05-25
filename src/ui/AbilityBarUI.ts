39eba56ecf429a36a6295c6e90abc296962167b9
YAGOTAGO <tiagocaetanodavies@gmail.com>
2026-05-25 18:03:26 -0400

    Update AbilityBarUI.ts

--- src/ui/AbilityBarUI.ts
+++ src/ui/AbilityBarUI.ts
@@ -9,7 +9,7 @@ export class AbilityBar extends GameObjects.Container {
         const screenWidth = scene.scale.width;
         const screenHeight = scene.scale.height;
         const x = screenWidth / 2;
-        const y = screenHeight - 100;
+        const y = screenHeight - 40;
         super(scene, x, y);
 
         console.log("Is RexUI available?", scene.rexUI); // Should log 'true'
@@ -18,12 +18,12 @@ export class AbilityBar extends GameObjects.Container {
 
         var gameObjects = [];
         for (var i = 0; i < 5; i++) {
-            gameObjects.push(scene.add.rectangle(0, 0, 50, 50, COLOR_LIGHT));
+            gameObjects.push(scene.add.rectangle(0, 0, 30, 30, COLOR_LIGHT));
         }
         const sizer = scene.rexUI.add.sizer({
             x: 0, 
             y: 0,
-            space: { left: 10, right: 10, top: 10, bottom: 10 }
+            space: { left: 5, right: 5, top: 5, bottom: 5 }
         })
         .addBackground(background)
         .addMultiple(
