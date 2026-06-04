3706bc61be37c080dfba5ddc41a167cc714b2951
Tiago Davies <tiagocaetanodavies@gmail.com>
2026-06-03 21:33:17 -0500

    Update config.prod.mjs

--- vite/config.prod.mjs
+++ vite/config.prod.mjs
@@ -17,7 +17,7 @@ const phasermsg = () => {
 }   
 
 export default defineConfig({
-    base: './rogue-survivor/',
+    base: '/rogue-survivor/',
     logLevel: 'warning',
     build: {
         rollupOptions: {
